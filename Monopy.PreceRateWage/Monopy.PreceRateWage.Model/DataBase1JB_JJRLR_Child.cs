e92067318e01659e1000878232bf66a3f80ee22a
DESKTOP-AUIIFE7\SunYu <2357839693@qq.com>
2018-05-31 17:44:57 +0800

    20080531 检包车间=>18.5/19

---
+++ Monopy.PreceRateWage/Monopy.PreceRateWage.Model/DataBase1JB_JJRLR_Child.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopy.PreceRateWage.Model
+{
+    public class DataBase1JB_JJRLR_Child
+    {
+        public Guid Id { get; set; }
+        public int No { get; set; }
+
+        /// <summary>
+        /// 类别名称
+        /// </summary>
+        public string CPMC { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public string Count { get; set; }
+
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public string Price { get; set; }
+    }
+}
