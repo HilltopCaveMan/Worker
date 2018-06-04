bb8549c51e2add59a4b057679707e644c7a09826
PV-X00163446\Administrator <2357839693@qq.com>
2018-06-04 22:37:49 +0800

    2018-06-04 烧成车间完成

---
+++ Monopy.PreceRateWage/Monopy.PreceRateWage.Model/DataBase1SC_HSY_BCKH.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopy.PreceRateWage.Model
+{
+    public class DataBase1SC_HSY_BCKH
+    {
+        public Guid Id { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string CreateUser { get; set; }
+        public int TheYear { get; set; }
+        public int TheMonth { get; set; }
+
+        /// <summary>
+        /// 开窑量
+        /// </summary>
+        public string KYL { get; set; }
+
+        /// <summary>
+        /// 修补不合格数
+        /// </summary>
+        public string XBBHGS { get; set; }
+
+        /// <summary>
+        /// 修补率
+        /// </summary>
+        public string XBL { get; set; }
+
+        /// <summary>
+        /// 目标
+        /// </summary>
+        public string MB { get; set; }
+
+        /// <summary>
+        /// 考核单价
+        /// </summary>
+        public string KHDJ { get; set; }
+
+        /// <summary>
+        /// 奖罚
+        /// </summary>
+        public string JF { get; set; }
+
+        /// <summary>
+        /// 考核金额
+        /// </summary>
+        public string  KHJJ { get; set; }
+    }
+}
