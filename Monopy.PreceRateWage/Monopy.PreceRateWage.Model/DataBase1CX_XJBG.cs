a2ad525bfa875f380d95bef5a8e01de58b4fa628
DESKTOP-AUIIFE7\SunYu <2357839693@qq.com>
2018-06-07 16:37:03 +0800

    2018-06-07 成型车间

---
+++ Monopy.PreceRateWage/Monopy.PreceRateWage.Model/DataBase1CX_XJBG.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monopy.PreceRateWage.Model
+{
+    /// <summary>
+    /// 小件报工
+    /// </summary>
+    public class DataBase1CX_XJBG
+    {
+        public Guid Id { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string CreateUser { get; set; }
+        public int TheYear { get; set; }
+        public int TheMonth { get; set; }
+
+        /// <summary>
+        /// 序号
+        /// </summary>
+        public string No { get; set; }
+
+        /// <summary>
+        /// 岗位
+        /// </summary>
+        public string GW { get; set; }
+
+        /// <summary>
+        /// 工号
+        /// </summary>
+        public string GH { get; set; }
+
+        /// <summary>
+        /// 人员编号
+        /// </summary>
+        public string UserCode { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public string LB { get; set; }
+
+        /// <summary>
+        /// 品种名称
+        /// </summary>
+        public string PZMC { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public string SL { get; set; }
+
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public string DJ { get; set; }
+
+        /// <summary>
+        /// 金额
+        /// </summary>
+        public string JE { get; set; }
+    }
+}
